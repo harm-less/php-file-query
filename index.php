1cfa09fb4d490ffbf62dd3315f5b40d9f0a44c75
Harm van der Werf <harmvdwerf@gmail.com>
2015-02-08 19:17:58 +0100

    Ignored .idea folder from PHPStorm

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 
 include_once __DIR__ . '/setup.php';
 $autoloader = require __DIR__ . '/vendor/autoload.php';
-//$autoloader->add('FQ\\Samples\\', __DIR__);
+$autoloader->add('FQ\\Samples\\', __DIR__);
 
 $sampleSimple = new \FQ\Samples\Simple();
 echo $sampleSimple->filePath('File1', 'child1');
